051c81843eb975c06777c02aaacb7a8b1e094366
Wonji1 <53804188+Wonji1@users.noreply.github.com>
2021-02-25 14:18:17 +0900

    21.02.25 수정

---
+++ src/com/my/model/DeleteEmailThread.java
@@ -0,0 +1,29 @@
+package com.my.model;
+
+import com.my.exception.RemoveException;
+import com.my.service.UserService;
+
+public class DeleteEmailThread extends Thread {
+    String email;
+
+    public DeleteEmailThread(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(30*1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        UserService service = new UserService();
+        try {
+            service.removeEmailById(email);
+            System.out.println(email + "삭제 완료");
+        } catch (RemoveException e) {
+            e.printStackTrace();
+            System.out.println("이미 tmp_email에 존재하지 않는 이메일");
+        }
+    }
+}
