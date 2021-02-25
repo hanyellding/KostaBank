5724e0e1353ec33620f7eb60ffefef5d9d8c3c63
Wonji1 <53804188+Wonji1@users.noreply.github.com>
2021-02-25 12:50:12 +0900

    Revert "Delete src/com/my/model directory"
    This reverts commit ddabb6ad6aab610a79289c4d44444082b01de96b.

---
+++ src/com/my/model/MyConfig.java
@@ -0,0 +1,25 @@
+package com.my.model;
+
+public class MyConfig {
+    private String email = "jih1092574@naver.com";
+    private String password = "zaq!1234";
+
+    public MyConfig() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
